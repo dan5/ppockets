889d7a4e00032e15e6d06e94a6ec9649d496e37c
dan5 <dan5.ya@gmail.com>
2011-02-01 08:02:23 +0900

    GameEnvironment追加

--- cron.rb
+++ cron.rb
@@ -15,6 +15,19 @@ Encoding.default_external = 'utf-8'
 
 DB = Sequel.sqlite
 
+class GameEnvironment < Sequel::Model
+  set_schema {
+    primary_key :id
+    Int :num_games, :default => 5
+    Int :stage, :default => 1
+  }
+  unless table_exists?
+    create_table
+    self.create
+  end
+end 
+GameEnv = DB[:game_environments].find(:id => 1)
+    
 class User < Sequel::Model
   one_to_one :player
   set_schema {
@@ -27,16 +40,18 @@ end
 class Player < Sequel::Model
   many_to_one :user
   one_to_many :cards
-  many_to_one :schedules
+  one_to_many :player
+  many_to_one :leagues
   set_schema {
     primary_key :id
     foreign_key :user_id, :users
-    foreign_key :schedule_id, :schedules
+    foreign_key :league_id, :leagues
     Bool :npc?, :default => false
     Bool :home?
     Bool :entry?
     Int :jewel, :default => 1000
     Int :num_commands, :default => 1
+    Int :stage
     Int :grade, :default => 0
     Int :points, :default => 0
     Timestamp :loged_at
@@ -51,9 +66,23 @@ class Player < Sequel::Model
   end
 
   def validate
-    raise if entry? && schedule_id
+    raise if entry? && league_id
   end
 end 
+
+class Result < Sequel::Model
+  many_to_one :player
+  set_schema {
+    primary_key :id
+    foreign_key :player_id, :players
+    Int :stage
+    Int :grade
+    Int :wins, :default => 0
+    Int :draws, :default => 0
+    Int :loses, :default => 0
+  }
+  create_table unless table_exists?
+end
 
 class Card < Sequel::Model
   many_to_one :player
@@ -84,87 +113,88 @@ class Card < Sequel::Model
   }
 end 
 
-class Schedule < Sequel::Model
+class League < Sequel::Model
   one_to_many :games
   one_to_many :players
   set_schema {
     primary_key :id
     Bool :ready?, :default => false
-    Int :day, :default => 0
-    Int :days_max
+    Int :game_count, :default => 0
+    Int :max_game_count
     #unique [:home_player_id, :away_player_id]
   }
   create_table unless table_exists?
 
   def next_game
-    games = Game.filter(:schedule_id => self.id).order(:day) # todo
+    games = Game.filter(:league_id => self.id).order(:game_count) # todo
     games.filter(:played? => false).limit(1).first
   end
 
   def dump_string
     "id: #{id}" +
-    games.map {|e| " day(#{e.day}, #{e.played? ? 'x' : '-'})" }.join(',')
+    games.map {|e| " game_count(#{e.game_count}, #{e.played? ? 'x' : '-'})" }.join(',')
   end
 end
       
 class Game < Sequel::Model
-  many_to_one :schedules
+  many_to_one :leagues
   set_schema {
     primary_key :id
-    foreign_key :schedule_id, :schedules
-    Int :day
+    foreign_key :league_id, :leagues
+    Int :game_count
     Bool :played?, :default => false
     Text :logs
     Timestamp :created_at
-    unique [:schedule_id, :day]
+    unique [:league_id, :game_count]
   }
   create_table unless table_exists?
 end
 
 # ----------------------------
-def create_schedules
+def create_leagues
   dump_method_name
-  days_max = 5
-  players = Player.filter(:schedule_id => nil).all
+  env = GameEnv.first
+  max_game_count = env[:num_games]
+  players = Player.filter(:league_id => nil).all
   while players.count >= 2 do
-    schedule = Schedule.create(:days_max => days_max)
-    puts "Schedule.create: id => #{schedule.id}"
-    players.shift.update(:schedule_id => schedule.id, :home? => true)
-    players.shift.update(:schedule_id => schedule.id, :home? => false)
-    days_max.times {|day| Game.create(:schedule_id => schedule.id, :day => day + 1) }
+    league = League.create(:max_game_count => max_game_count)
+    puts "League.create: id => #{league.id}"
+    players.shift.update(:stage => env[:stage], :league_id => league.id, :home? => true)
+    players.shift.update(:stage => env[:stage], :league_id => league.id, :home? => false)
+    max_game_count.times {|game_count| Game.create(:league_id => league.id, :game_count => game_count + 1) }
   end
   p players.count
 end
 
-def delete_schedules
+def delete_leagues
   dump_method_name
-  Schedule.all.each do |schedule|
-    next if schedule.next_game
-    Player.filter(:schedule_id => schedule.id).update(:schedule_id => nil)
-    Game.filter(:schedule_id => schedule.id).delete # todo: 自動的に消えてほしい
-    schedule.delete
+  League.all.each do |league|
+    next if league.next_game
+    Player.filter(:league_id => league.id).update(:league_id => nil)
+    Game.filter(:league_id => league.id).delete # todo: 自動的に消えてほしい
+    league.delete
   end
 end
 
-def update_schedules
+def update_leagues
   dump_method_name
-  Schedule.all.each do |schedule|
-    schedule.update(:day => schedule.day + 1) # todo
+  League.all.each do |league|
+    league.update(:game_count => league.game_count + 1) # todo
   end
-  p Schedule.all
+  p League.all
 end
 
 def do_games
   dump_method_name
-  Schedule.filter('day > 0').each do |schedule|
-    game = schedule.next_game
+  League.filter('game_count > 0').each do |league|
+    game = league.next_game
     game.update(:played? => true)
   end
 end
 
 # -- debug ---------------------
-def debug_dump_schedules
-  puts Schedule.all.map(&:dump_string)
+def debug_dump_leagues
+  puts League.all.map(&:dump_string)
 end
 
 def debug_create_players(n)
@@ -177,9 +207,9 @@ end
 debug_create_players(7)
 
 15.times do
-  create_schedules
+  create_leagues
   do_games
-  debug_dump_schedules
-  delete_schedules
-  update_schedules
+  debug_dump_leagues
+  delete_leagues
+  update_leagues
 end
